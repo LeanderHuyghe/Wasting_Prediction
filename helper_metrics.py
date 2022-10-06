f529bbd4ad6ca75b46a9209d713b814d0202bffd
pradyutnair <p.nair@student.tue.nl>
2022-10-06 17:21:21 +0200

    Added Maja's missing value count functions

---
+++ helper_metrics.py
@@ -0,0 +1,198 @@
+# Imports
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import precision_recall_fscore_support
+import pickle
+import numpy as np
+import itertools
+
+
+########################################################################################################
+# ROC CURVE
+def roc_curve_gen(y_true, y_pred, model_name="Model"):
+    """
+    Parameters
+    ----------
+    y_true
+    y_pred
+    model_name: for title of plot
+    Returns
+    -------
+    ROC curve
+    """
+    fpr, tpr, threshold = roc_curve(y_true, y_pred)
+
+    plt.plot(fpr, tpr, label="Predicted Increase")
+
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'ROC Curve for {model_name}')
+    plt.legend(loc="lower right", prop={'size': 8})
+    plt.show()
+
+
+########################################################################################################
+# CONFUSION MATRIX
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False,
+                          cmap=plt.cm.Blues):
+    """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+    If classes is passed, confusion matrix will be labelled, if not, integer class values
+    will be used.
+    Args:
+      y_true: Array of truth labels (must be same shape as y_pred).
+      y_pred: Array of predicted labels (must be same shape as y_true).
+      classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+      figsize: Size of output figure (default=(10, 10)).
+      text_size: Size of output figure text (default=15).
+      norm: normalize values or not (default=False).
+      savefig: save confusion matrix to file (default=False).
+    Returns:
+      A labelled confusion matrix plot comparing y_true and y_pred.
+    Example usage:
+      make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                            y_pred=y_preds, # predicted labels
+                            classes=class_names, # array of class label names
+                            figsize=(15, 15),
+                            text_size=10)
+    Parameters
+    ----------
+    cmap
+    """
+    # Create the confustion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]  # normalize it
+    n_classes = cm.shape[0]  # find the number of classes we're dealing with
+
+    # Plot the figure and make it pretty
+    fig, ax = plt.subplots(figsize=figsize)
+    cax = ax.matshow(cm, cmap=cmap)  # colors will represent how 'correct' a class is, darker == better
+    fig.colorbar(cax)
+
+    # Are there a list of classes?
+    if classes:
+        labels = classes
+    else:
+        labels = np.arange(cm.shape[0])
+
+    # Label the axes
+    ax.set(title="Confusion Matrix",
+           xlabel="Predicted label",
+           ylabel="True label",
+           xticks=np.arange(n_classes),  # create enough axis slots for each class
+           yticks=np.arange(n_classes),
+           xticklabels=labels,  # axes will labeled with class names (if they exist) or ints
+           yticklabels=labels)
+
+    # Make x-axis labels appear on bottom
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+
+    # Set the threshold for different colors
+    threshold = (cm.max() + cm.min()) / 2.
+
+    # Plot the text on each cell
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if norm:
+            plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j] * 100:.1f}%)",
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > threshold else "black",
+                     size=text_size)
+        else:
+            plt.text(j, i, f"{cm[i, j]}",
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > threshold else "black",
+                     size=text_size)
+
+    # Save the figure to the current working directory
+    if savefig:
+        fig.savefig("confusion_matrix.png")
+
+
+########################################################################################################
+# SAVE MODEL
+def save_model(model, filename):
+    """
+    Parameters
+    ----------
+    model: sklearn model
+    filename: should be a string in the format "model_name.sav"
+    Returns
+    -------
+    saved pkl model
+    """
+    pickle.dump(model, open(filename, 'wb'))
+
+
+########################################################################################################
+# CALCULATE METRICS
+def calculate_results(y_true, y_pred, average="weighted"):
+    """
+    Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+    Args:
+        y_true: true labels in the form of a 1D array
+        y_pred: predicted labels in the form of a 1D array
+    Returns a dictionary of accuracy, precision, recall, f1-score.
+    """
+    # Calculate model accuracy
+    model_accuracy = accuracy_score(y_true, y_pred) * 100
+    # Calculate model precision, recall and f1 score using "weighted average
+    model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average=average)
+    model_results = {"accuracy": model_accuracy,
+                     "precision": model_precision,
+                     "recall": model_recall,
+                     "f1": model_f1}
+    return model_results
+
+
+########################################################################################################
+# PLOT TIME SERIES DATA
+def plot_time_series(timesteps, values, format='-', start=0, end=None, label=None):
+    """
+      Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+      Parameters
+      ---------
+      timesteps : array of timesteps
+      values : array of values across time
+      format : style of plot, default "."
+      start : where to start the plot (setting a value will index from start of timesteps & values)
+      end : where to end the plot (setting a value will index from end of timesteps & values)
+      label : label to show on plot of values
+    """
+    # Plot the series
+    plt.plot(timesteps[start:end], values[start:end], format, label=label)
+    plt.xlabel("Date")
+    plt.ylabel("Next Prevalence")
+    if label:
+        plt.legend(fontsize=14)  # make label bigger
+    plt.grid(True)
+
+########################################################################################################
+# CALCULATE MISSING VALUES
+def count_missing_district(df):
+
+    """ This function shows the total number of missing values in each column
+    per district"""
+
+    df = df.groupby('district')
+    df = df.count().rsub(df.size(),axis=0)
+    return df
+
+def count_missing_district_total(df):
+
+    """This function shows the total number of missing values per district
+     """
+
+    df = df.groupby('district')
+    df = df.count().rsub(df.size(), axis=0)
+    df.reset_index()
+    df = df.sum(axis=1)
+    return df
+########################################################################################################
